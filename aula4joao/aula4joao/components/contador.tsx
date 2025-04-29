1e8b96e4f92f64585534b582471352d2f21237a1
Gustavo <gustavo.souza0911@gmail.com>
2025-04-29 17:53:38 -0300

    Entrega arquivo completo

--- aula4joao/aula4joao/components/contador.tsx
+++ aula4joao/aula4joao/components/contador.tsx
@@ -1,16 +1,54 @@
-import React from 'react';
-import {Text, Button,View, StyleSheet} from 'react-native';
+import React from "react";
+import { Text, Button, View, StyleSheet } from
+'react-native';
 
-export default class Contador extends React.Component{
+export default class Contador extends React.Component {
     constructor() {
         super();
         this.state = {
-            contador : 0,
+            contador: 0,   
         };
     }
-    Decrementar(){
+    Decrementar() {
         this.setState({
             contador: this.state.contador - 1
         });
     }
-}
+    Incrementar() {
+        this.setState({
+            contador: this.state.contador + 1
+        });
+    }
+render() {
+    return (
+        <View>
+            <View style={styles.texto}>
+                <Text>{this.state.contador}</Text>
+            </View>
+            <View style={styles.botoes}>
+                <Button
+                  style={styles=botao1}
+                  title="Aumenta"
+                  onPress={this.Incrementar.bind(this)}
+                />
+                <Button
+                  style={styles.botao2}
+                  onPress={this.Decrementar.bind(this)}
+                  title="Diminuir"/>
+            </View>
+        </View>
+    );  } }
+
+const styles = StyleSheet.create({
+    botoes: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignSelf: 'center',
+        top: 220,
+        height: 40,
+    },
+    texto: {
+        alignItems: 'center',
+        top: 200,
+    },
+});
